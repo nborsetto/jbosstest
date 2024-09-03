8a5720833ea653af5166fb49f89544a815b1d45f
nicola_borsetto <Nicola_Borsetto@secservizi.it>
2024-09-03 15:28:42 +0200

    New branch

---
+++ testjsf/src/main/java/org/jboss/as/quickstarts/numberguess/HelloBean.java
@@ -0,0 +1,31 @@
+package org.jboss.as.quickstarts.numberguess;
+
+import java.io.Serializable;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@RequestScoped
+@Named
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 3344199387523339981L;
+	private String name;
+	private String greeting;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void doGreeting() {
+		greeting = "Ciao: " + name;
+	}
+}
